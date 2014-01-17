96f5a511026329f3309105bfe7c54b510d3f7132
Harrison Chen <harrisonxchen@gmail.com>
2014-01-17 06:40:23 -0800

    Chnage initial signup/signin redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   		if @user.save
   			sign_in @user
   			flash[:success] = "Welcome to What-ToDo!"
-  			redirect_to @user
+  			redirect_to lists_path
   		else
   			render 'new'
   		end
